bf25288e15336a2702e7371f73ee3555401d7726
[mubleen] <102492190+Mublin@users.noreply.github.com>
2025-06-14 10:01:12 +0100

    jwt guard

---
+++ src/auth/Guards/jwt.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Observable } from "rxjs";
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('Jwt') implements CanActivate{
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        return super.canActivate(context)
+    }
+}
