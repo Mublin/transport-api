aa1fabd55c85508a23fb8035e35f70d1faddc3d6
[mubleen] <102492190+Mublin@users.noreply.github.com>
2025-06-14 09:35:49 +0100

    added refreshtoken and access token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports:[
+    PassportModule,
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) =>{
+        return (
+          {
+            secret: configService.get("JWT_SECRET"),
+            signOptions: {
+              expiresIn: '7d'
+            }
+          }
+        )
+      },
+      inject: [ConfigService]
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,143 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { Role } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async validateUser({
+    email,
+    inputPassword,
+  }: {
+    email: string;
+    inputPassword: string;
+  }) {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      throw new NotFoundException('Invalid creds');
+    }
+    let verify = await bcrypt.compare(inputPassword, user.password);
+    if (!verify) {
+      throw new NotFoundException('Invalid creds');
+    }
+    return {
+      _id: user._id,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      firstName: user.firstName,
+      role: user.role,
+    };
+  }
+
+  async create(createAuthDto: CreateAuthDto) {
+    const { password, ...creds } = createAuthDto;
+    const hashed = await bcrypt.hash(password, 12);
+    const user: any = await this.userService.create({
+      password: hashed,
+      ...creds,
+    });
+    const refreshToken = await this.updateRefreshToken(user._id, {
+      _id: user._id,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      firstName: user.firstName,
+      role: user.role,
+    });
+    return {
+      accessToken: this.jwtService.sign({
+        sub: user._id,
+        role: user.role,
+        email: user.email,
+      }),
+      refreshToken: refreshToken,
+      email: user.email,
+      role: user.role,
+    };
+  }
+
+  async login({
+    identifier,
+    inputPassword,
+  }: {
+    identifier: string;
+    inputPassword: string;
+  }) {
+    try {
+      const user: any = await this.validateUser({
+        email: identifier,
+        inputPassword,
+      });
+      if (!user) {
+        throw new Error('error 3');
+      }
+      const refreshToken = await this.updateRefreshToken(user._id, {
+        _id: user._id,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        firstName: user.firstName,
+        role: user.role,
+      });
+      return {
+        accessToken: this.jwtService.sign({
+          sub: user._id,
+          role: user.role,
+          email: user.email,
+        }),
+        refreshToken: refreshToken,
+        email: user.email,
+        role: user.role,
+      };
+    } catch (error) {
+      throw new NotFoundException(error);
+    }
   }
 
   findAll() {
     return `This action returns all auth`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} auth`;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  async updateRefreshToken(
+    id: string,
+    user: {
+      _id: string;
+      email: string;
+      phoneNumber: string;
+      firstName: string;
+      role: Role;
+    },
+  ) {
+    const refreshToken = await this.jwtService.signAsync(
+      {
+        sub: user._id,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        firstName: user.firstName,
+        role: user.role,
+      },
+      {
+        secret: this.configService.get('JWT_REFRESH'),
+        expiresIn: '2d',
+      },
+    );
+    await this.userService.updateRefreshToken(id, refreshToken);
+    return refreshToken;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} auth`;
   }
 }
