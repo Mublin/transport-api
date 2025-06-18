ab36b26768cd413212c67245e8fe65f163e65400
[mubleen] <102492190+Mublin@users.noreply.github.com>
2025-06-18 09:04:54 +0100

    delivery module

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,34 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
-import { UpdateDeliveryDto } from './dto/update-delivery.dto';
+import { UpdateDeliveryStatusDto } from './dto/update-delivery.dto';
 
 @Controller('delivery')
 export class DeliveryController {
   constructor(private readonly deliveryService: DeliveryService) {}
 
   @Post()
-  create(@Body() createDeliveryDto: CreateDeliveryDto) {
-    return this.deliveryService.create(createDeliveryDto);
+  async createDelivery(@Request() req, @Body() body: CreateDeliveryDto) {
+    const userId = "684be10fa8c39fc1f443db82" 
+    // || req.user.userId;
+    const { pickupLocation, dropoffLocation } = body;
+    return this.deliveryService.createDelivery(userId, pickupLocation, dropoffLocation);
   }
 
-  @Get()
-  findAll() {
-    return this.deliveryService.findAll();
+  @Patch(':id/status')
+  async updateDeliveryStatus(@Request() req, @Param('id') id: string, @Body() body: UpdateDeliveryStatusDto) {
+    const driverId = req.user.role === 'driver' ? req.user.userId : undefined;
+    return this.deliveryService.updateDeliveryStatus(id, body.status, driverId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.deliveryService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDeliveryDto: UpdateDeliveryDto) {
-    return this.deliveryService.update(+id, updateDeliveryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.deliveryService.remove(+id);
+  async getDelivery(@Param('id') id: string) {
+    return this.deliveryService.getDelivery(id);
   }
 }
--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -3,10 +3,14 @@ import { DeliveryService } from './delivery.service';
 import { DeliveryController } from './delivery.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Delivery, DeliverySchema } from './schema/delivery.schema';
+import { OpenrouteModule } from 'src/openroute/openroute.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{name: Delivery.name, schema: DeliverySchema}])
+    MongooseModule.forFeature([{name: Delivery.name, schema: DeliverySchema}]),
+    OpenrouteModule,
+    UsersModule
   ],
   controllers: [DeliveryController],
   providers: [DeliveryService],
